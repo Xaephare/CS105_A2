aa874cbb434453d14583fed9660c0de30ca9dc35
Xaephare <louisgrace@onmail.com>
2022-09-20 15:33:25 +1200

    +added display function +defined constructors

--- S2_Q3/complex.cpp
+++ S2_Q3/complex.cpp
@@ -1,31 +1,37 @@
 #include "complex.h"
 
+
 Complex::Complex() {
-	
+	real = 3;
+	imaginary = 2;
 }
 
-Complex::Complex(int real, int imaginary) {
-	
+Complex::Complex(int r, int i) {
+	real = r;
+	imaginary = i;
 }
 
 void Complex::display_complex() {
-	
+	std::cout << this->real << this->imaginary << "i";
 }
 
 Complex Complex::operator+(Complex const& obj) {
 	Complex res;
 	res.real = this->real + obj.real;
 	res.imaginary = this->imaginary + obj.imaginary;
+	return res;
 }
 
 Complex Complex::operator-(Complex const& obj) {
 	Complex res;
 	res.real = this->real - obj.real;
 	res.imaginary = this->imaginary - obj.imaginary;
+	return res;
 }
 
 Complex Complex::operator*(Complex const& obj) {
 	Complex res;
 	res.real = (this->real * obj.real) - (this->imaginary * obj.imaginary);
 	res.imaginary = (this->real * obj.imaginary) + (this->imaginary * obj.real);
+	return res;
 }
