f88284d707cd6930fcf1ce738a51eff68ec4ed24
Xaephare <louisgrace@onmail.com>
2022-09-20 15:53:18 +1200

    +added a function for creating new complex numbers.

--- S2_Q3/main.cpp
+++ S2_Q3/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include "complex.h"
+
+
+Complex create_new_complex() {
+	int real = 0;
+	int imaginary = 0;
+	
+	std::cout << "Enter 2nd Complex number values:\n";
+	std::cout << "Enter Real value: ";
+	std::cin >> real;
+	std::cout << "Enter Imaginary value: ";
+	std::cin >> imaginary;
+
+	Complex new_complex(real, imaginary);
+	return new_complex;
+}
 
 
 int main() {
+	
+	Complex num1;
+	Complex num2 = create_new_complex();
+
+	num1.display_complex();
+	num2.display_complex();
 	
 	
 	
